bf1876d9ed8b805c6823ac1d0de6587603ea0076
sheela senga <SSengamalam@spartaglobal.com>
2019-09-18 20:18:06 +0100

    commit

---
+++ FirstASPDotNetMVCApp/Controllers/DefaultController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstASPDotNetMVCApp.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Home()
+        {
+            ViewBag.Message = "Your home page.";
+
+            return View();
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ FirstASPDotNetMVCApp/Models/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FirstASPDotNetMVCApp.Models
+{
+    public class Customer
+    {
+        private string firstname = "";
+        private string surname = "";
+        private int age = 0;
+
+        public string Firstname { get => firstname; set => firstname = value; }
+        public string Surname { get => surname; set => surname = value; }
+        public int Age { get => age; set => age = value; }
+    }
+}
